1eecd213188d747fab10c3b4ab08f66ae399ea87
e-kondr01 <e.kondr01@gmail.com>
2022-03-12 19:14:00 +0300

    [feat] add user retrieve endpoint

--- homeworks/K33402/Kondrashov_Egor/HW2/index.js
+++ homeworks/K33402/Kondrashov_Egor/HW2/index.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/users/:email', async (req, res) => {
+    const user = await db.User.findOne({ where: { email: req.params.email } })
+    let resp = null;
+
+    if (user) {
+        resp = user.toJSON()
+
+    } else { resp = { "msg": "user not found" } }
+    res.send(resp)
+})
+
 app.listen(port, () => {
     console.log(`Starting local server on port ${port}`)
 })
