fe4a5ebc49504e004814f2af732bd6fcc7c2f8f8
Yurii Shuginin <yurii.shuginin@yandex.ru>
2022-04-14 00:52:00 +0300

    Init Sequelize ORM & implement user model with id get endpoint

--- homeworks/K33402/Shuginin_Yurii/HW2/index.js
+++ homeworks/K33402/Shuginin_Yurii/HW2/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const db = require('./models')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/users/:id', async (req, res) => {
+  const user = await db.User.findByPk(req.params.id)
+  console.log('user is', user)
+
+  if (user) {
+    res.send(user.toJSON())
+  } else {
+    res.send({"msg": "user is not found"})
+  }
 })
 
 app.listen(port, () => {
