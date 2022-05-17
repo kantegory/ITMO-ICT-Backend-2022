e7c8c10a958b437c9aa768ca642e910c23aec658
gabrieleger <55911885+gabrieleger@users.noreply.github.com>
2022-05-17 18:27:48 +0300

    add LR1 HW3

---
+++ labs/K33402/Gerulaityte Gabriele/LR1/src/routes/index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import Controller from '../controllers/user'
+import { Router } from 'express'
+
+
+const router: express.Router = express.Router()
+
+const controller = new Controller()
+
+router.route('/read')
+  .get(controller.get)
+
+router.route('/create')
+  .post(controller.post)
+
+router.route('/user/:id')
+  .get(controller.getbyID)
+
+router.route('/update/:id')
+  .put(controller.put)
+
+router.route('/delete/:id')
+  .delete(controller.delete)
+  
+export default router
