d75cd2c2fdd3fea28e9339f57aed696b145d5b7a
DaniilShutov <56311745+Daniil19112001@users.noreply.github.com>
2022-06-01 17:46:19 +0300

    add LR3 & LR4

---
+++ labs/K33402/Shutov_Daniil/LR4/login/controllers/login.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+export default class LoginController {
+
+    login = async (request: any, response: any) => {
+        const email = request.body.email
+        const password = request.body.password
+        if (email && password) {
+            let result = await axios.get("http://lr4_other_1:8000/api/getUserByEmail?email="+email)
+            let user = result.data
+            if (!user) {
+                response.status(401).json({ status: 'User not found', user })
+            } else {
+                if (user!!.password === password) {
+                    let payload = { id: user!.id }
+                    const jwt = require('jsonwebtoken')
+                    let token = jwt.sign(payload, "secretKey")
+                    response.json({ status: 'Success', token: token })
+                } else {
+                    response.status(401).json({ status: 'Password is incorrect' })
+                }
+            }
+        }
+    }
+}
