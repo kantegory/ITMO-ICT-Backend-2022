230363b91106447c941c00d0bda382986ab24cee
Le Lan Anh <56070041+lananhnobi@users.noreply.github.com>
2022-06-03 15:11:19 +0300

    lap 4 done

---
+++ labs/K33402/Lan_anh/Lap 4/customer/src/config/index.ts
@@ -0,0 +1,19 @@
+const dotEnv  = require("dotenv");
+
+if (process.env.NODE_ENV !== 'prod') {
+    const configFile =  `./.env`;
+    dotEnv.config({ path:  configFile });
+} else {
+    dotEnv.config();
+}
+
+module.exports = {
+    PORT: process.env.PORT,
+    DB_URL: process.env.MONGODB_URI,
+    APP_SECRET: process.env.APP_SECRET,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME: 'ONLINE_BOOKING',
+    CUSTOMER_BINDING_KEY: 'CUSTOMER_SERVICE',
+    QUEUE_NAME: 'CUSTOMER_QUEUE'
+}
+ 
---
+++ labs/K33402/Lan_anh/Lap 4/customer/src/database/connection.ts
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { DB_URL } = require('../config/index');
+
+module.exports = async() => {
+    try {
+        await mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Db Connected');
+        
+    } catch (error) {
+        console.log('Error ============')
+        console.log(error);
+        process.exit(1);
+    }
+ 
+};
+
+ 
