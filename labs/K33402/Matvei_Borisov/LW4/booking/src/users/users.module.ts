a3b391d65e0c9e9bb574fd3a3923110be32dc109
Matvei Borisov <bme15122000@gmail.com>
2022-06-09 01:40:56 +0300

    done lw4

---
+++ labs/K33402/Matvei_Borisov/LW4/booking/src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./entities/user.entity";
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [UsersService]
+})
+export class UsersModule {}
