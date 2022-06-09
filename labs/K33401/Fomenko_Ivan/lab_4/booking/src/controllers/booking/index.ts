a1de5478b3e758a656698bcc8a486f261a5ea471
Ivan <ivanivanfomenko@gmail.com>
2022-06-10 01:10:14 +0300

    add check for space

--- labs/K33401/Fomenko_Ivan/lab_4/booking/src/controllers/booking/index.ts
+++ labs/K33401/Fomenko_Ivan/lab_4/booking/src/controllers/booking/index.ts
@@ -1,13 +1,15 @@
 import BookingService from "../../services/booking"
 import UserService from "../../services/user"
+import HotelService from "../../services/hotel"
 
 class BookingController {
-    private service = new BookingService()
-    private service2 = new UserService()
+    private bookingService = new BookingService()
+    private userService = new UserService()
+    private hotelService = new HotelService()
 
     get = async (request: any, response: any) => {
         try{
-            const data = await this.service.getBookings(request.user.id)
+            const data = await this.bookingService.getBookings(request.user.id)
             response.send(data)
         } catch(error: any){
             response.status(400).send(error.message)
@@ -18,12 +20,13 @@ class BookingController {
         try{
             const booking = request.body
             booking.userId = request.user.id
-            const usero = await this.service2.getById(booking.userId)
-            if(usero && usero.age > 17){
-                await this.service.add(booking.arrival, booking.departure, booking.visitors, booking.userId, booking.hotelId)
-                response.send('Successfully added user')
+            const usero = await this.userService.getById(booking.userId)
+            const hotelo = await this.hotelService.getById(booking.hotelId)
+            if(usero && usero.age > 17 && hotelo && hotelo.capacity > booking.visitors){
+                await this.bookingService.add(booking.arrival, booking.departure, booking.visitors, booking.userId, booking.hotelId)
+                response.send('Successfully added booking')
             } else {
-                response.status(400).send('Too young for that')
+                response.status(400).send('Out of space or you are too young')
             }
         } catch(error: any){
             response.status(400).send(error.message)
--- labs/K33401/Fomenko_Ivan/lab_4/booking/src/services/hotel/index.ts
+++ labs/K33401/Fomenko_Ivan/lab_4/booking/src/services/hotel/index.ts
@@ -12,6 +12,10 @@ class HotelService {
         return this.repo.findAll()
     }
 
+    getById(hotel_id: number){
+        return this.repo.findOne({where: {id: hotel_id}})
+    }
+
     getWithParameters(hotel_town: string, hotel_type: string){
         const { Op } = require("sequelize")
         return this.repo.findAll({where: {town: hotel_town, type: hotel_type, capacity: {[Op.gte]: 1}}})
