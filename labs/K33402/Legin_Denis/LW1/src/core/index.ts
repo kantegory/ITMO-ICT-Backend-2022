b35e57c5a31132c6f7f38a8f5798899a600f2e0a
aNOOBis666 <mr.zooko@mail.ru>
2022-06-13 22:53:43 +0300

    Legin_Denis_k33402_LW1_updated

--- labs/K33402/Legin_Denis/LW1/src/core/index.ts
+++ labs/K33402/Legin_Denis/LW1/src/core/index.ts
@@ -6,19 +6,19 @@ import { sequelize } from '../config/config'
 class App {
     public port: number
     public host: string
-  
+
     private app: express.Application
     private server: Server
 
     constructor(port = 8008, host = "localhost") {
         this.port = port
         this.host = host
-    
-        this.app = this.createApp()
+
+        this.app = App.createApp()
         this.server = this.createServer()
     }
-    
-    private createApp(): express.Application {
+
+    private static createApp(): express.Application {
         const app = express()
         const bodyParser = require('body-parser')
         app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,16 +26,18 @@ class App {
         app.use('/v1', userRoutes)
         return app
       }
-    
+
     private createServer(): Server {
         return createServer(this.app)
     }
 
     public start(): void {
         sequelize.sync().then(() => {
+            // tslint:disable-next-line:no-console
             console.log('DB connected')
         })
         this.server.listen(this.port, () => {
+            // tslint:disable-next-line:no-console
             console.log(`Running server on port ${this.port}`)
         })
     }
