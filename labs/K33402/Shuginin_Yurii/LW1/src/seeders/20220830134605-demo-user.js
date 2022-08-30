af141bc2184a91ed76421f0a6aa08a82f532809e
Yurii Shuginin <yurii.shuginin@yandex.ru>
2022-08-30 19:58:48 +0300

    Add User seeder

---
+++ labs/K33402/Shuginin_Yurii/LW1/src/seeders/20220830134605-demo-user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{
+      email: 'demo1@ya.ru',
+      password: '12345',
+      firstName: 'John',
+      lastName: 'Doe',
+      birthday: '2003-02-01',
+      nationality: 'England',
+      passport: '1234567890',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
