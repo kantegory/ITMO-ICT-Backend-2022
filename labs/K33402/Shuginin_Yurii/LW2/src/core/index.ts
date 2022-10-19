434b2b3cea260b7631c614e8aa3a2f60a030ecd7
Yurii Shuginin <yurii.shuginin@yandex.ru>
2022-10-19 15:17:57 +0300

    Initial boilerplate

---
+++ labs/K33402/Shuginin_Yurii/LW2/src/core/index.ts
@@ -0,0 +1,52 @@
+import express from "express"
+import { createServer, Server } from "http"
+import userRoutes from "../routes/users/UserRoutes"
+import sequelize from "../providers/db"
+import { Sequelize } from 'sequelize-typescript'
+
+class App {
+  public port: number
+  public host: string
+
+  private app: express.Application
+  private server: Server
+  private sequelize: Sequelize
+
+  constructor(port = 8000, host = "localhost") {
+      this.port = port
+      this.host = host
+  
+      this.app = this.createApp()
+      this.server = this.createServer()
+      this.sequelize = sequelize
+  }
+  
+  private createApp(): express.Application {
+      const app = express()
+      app.use(express.json())
+      app.use('/users', userRoutes)
+  
+      return app
+    }
+  
+  private createServer(): Server {
+      const server = createServer(this.app)
+  
+      return server
+  }
+
+  public start(): void {
+      this.sequelize
+        .sync()
+        .then(() => {
+          console.log('Models synchronized successfully')
+        })
+        .catch((error) => console.log(error));  
+
+      this.server.listen(this.port, () => {
+          console.log(`Running server on port ${this.port}`)
+      })
+  }
+}
+
+export default App
---
+++ labs/K33402/Shuginin_Yurii/LW2/src/services/users/UserService.ts
@@ -0,0 +1,54 @@
+import User from "../../models/User"
+
+class UserService {
+    async getAllUsers() : Promise<User[]> {
+        const users = await User.findAll()
+        return users
+    }
+
+    async getUserByID(id: number) : Promise<User|Error> {
+        const user = await User.findByPk(id);
+        if (user) {
+            return user.toJSON()
+        }
+        throw new Error('No User with such id')
+    }
+
+    async createUser(userData: any) : Promise<User|Error> {
+        try {
+            const user = await User.create(userData)
+            return user.toJSON()
+        } catch (error: any) {
+            throw new Error(error)
+        }
+    }
+
+    async updateUser(userData: any) : Promise<User|Error> {
+        try {
+            const user = await User.findByPk(userData.id)
+            if (user) {
+                await user.update(userData, { where: { id: userData.id } })
+                return user.toJSON()
+            } else {
+                throw new Error('No user with such id')
+            }
+        } catch (error: any) {
+            throw new Error(error)
+        }
+    }
+
+    async deleteUser(id: number) : Promise<void|Error> {
+        try {
+            const user = await User.findByPk(id)
+            if (user) {
+                user.destroy()
+            } else {
+                throw new Error('No user with such id')
+            }
+        } catch (error: any) {
+            throw new Error(error)
+        }
+    }
+}
+
+export default UserService
